d3f971dc2dfc34acb470d59c20ad6d8ea8d7c909
TechTheAwesome <techtheawesome01@gmail.com>
2022-04-26 18:38:07 +0900

    ADD tailwind darkmode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{html,tsx,ts,css}"],
   theme: {
     extend: {},
+    container: {
+      center: true
+    }
   },
   plugins: [],
+  darkMode: 'media',
 }
